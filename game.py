06cb06b2baa8bfbe1987bcb1b21c91d324baa72c
amogh-dongre <amoghdongre16@gmail.com>
2021-05-26 10:48:14 +0530

    changing the files

--- game.py
+++ game.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python3
-#Hiya this is a program to guess and win
-#importing all of the functions
+# !/usr/bin/env python3
+# Hiya this is a program to guess and win
 import random
 import math
-#Inputing of the lower limit
+
 ll=int(input("Enter the lower limit "))
-#Inputing of the upper limit
+
 ul=int(input("Enter the upper limit "))
-#now creating a random number
+
 random=random.randint(ll,ul)
 print(f'\n you have  got {round(math.log(ul - ll + 1 , 2))} number of chances to guess the correct number!')
+
 count=0
 while count<math.log(ul-ll+1,2):
    count+=1
@@ -21,5 +21,6 @@ while count<math.log(ul-ll+1,2):
       print(f"youre guess is too low guess again :) ")
    elif random>guess:
       print(f"youre guess is too high guess again :) ")
+
 if count>=math.log(ul-ll+1,2):
     print(f"The number is {random} better luck next time")
